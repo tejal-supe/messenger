0c15bff24859b4387590c4f5c6c030fb97ccc62f
tejal-supe <supetejalc@gmail.com>
2024-02-26 17:42:45 +0530

    CSS

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -49,17 +49,40 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow-md sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          {
-            variant==='REGISTER' && (
-              <Input label="name" register={register} id="name" errors={errors} />
-
-            )
-          }
-           <Input label="Email" register={register} id="Email" errors={errors} type="email"/> <Input label="Password" register={register} id="Pass" errors={errors} type="password"/>
-           <div>
-            <Button>Submit</Button>
-           </div>
+          {variant === "REGISTER" && (
+            <Input label="name" register={register} id="name" errors={errors} />
+          )}
+          <Input
+            label="Email"
+            register={register}
+            id="Email"
+            errors={errors}
+            type="email"
+          />{" "}
+          <Input
+            label="Password"
+            register={register}
+            id="Pass"
+            errors={errors}
+            type="password"
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Resgister"}
+            </Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" ></div>
+              <div className="relative flex justify-center text-sm">
+                <span className="bg-white px-2 text-gray-500"> OR Continue with</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
